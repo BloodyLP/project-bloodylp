6e7cb5b9b2390d904c6cc181276b6bf85dc4dd45
Alexander Blattmann <info@bloodylp.de>
2026-09-06 19:35:18 +0200

    pOSITION gEÄNDERT

--- components/live/data/nhl27/player-profile.ts
+++ components/live/data/nhl27/player-profile.ts
@@ -15,7 +15,7 @@ export type PlayerProfile = {
 
 export const playerProfile: PlayerProfile = {
     name: "Alexander Blattmann",
-    position: "CENTER",
+    position: "LINKER FLÜGEL",
     number: 15,
     birthPlace: "Freiburg im Breisgau, Deutschland",
     birthDate: "13. Juli 2008",
